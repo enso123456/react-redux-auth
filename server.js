29a89504e509774a584c677b3de90b718750b1da
Romeo Enso <enso123456@gmail.com>
2018-04-17 20:08:35 +0800

    modify server

--- server.js
+++ server.js
@@ -4,8 +4,4 @@ const app = express();
 
 app.use(express.static(__dirname + "/"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
-});
-
 app.listen(process.env.PORT || 8080);
