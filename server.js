c88714926aabb75893471c0de8635a4c27dbcf75
Romeo Enso <enso123456@gmail.com>
2018-04-17 20:18:50 +0800

    server index.html

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const app = express();
 
 app.use(express.static(__dirname + "/"));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
 app.listen(process.env.PORT || 8080);
